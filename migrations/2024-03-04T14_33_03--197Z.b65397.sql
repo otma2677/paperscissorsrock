25add840ba43ab8e3cb7ba30f8e7f1cf281df1fb
eiwos <mahmoudi.otman@gmail.com>
2024-03-05 13:24:01 +0100

    New migration

---
+++ migrations/2024-03-04T14_33_03--197Z.b65397.sql
@@ -0,0 +1,3 @@
+#Generated at through the script C:\Users\mahmo\WebstormProjects\paperscissorsrock\bin\create-migration-file.js at 2024-03-04T14:33:03.199Z.
+
+alter table messages add column ip_address varchar(64);
