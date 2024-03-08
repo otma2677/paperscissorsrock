aad9c326cb1d415c1eec1563bf31124f3c5b128a
eiwos <mahmoudi.otman@gmail.com>
2024-03-08 15:59:12 +0100

    adding support for duplicate entries in connections, upsert did not work because of unique

---
+++ migrations/2024-03-08T14_56_01--202Z.3a7872.sql
@@ -0,0 +1,3 @@
+#Generated at through the script C:\Users\mahmo\WebstormProjects\paperscissorsrock\bin\create-migration-file.js at 2024-03-08T14:56:01.205Z.
+#
+alter table connections modify column user_id int unsigned unique;
