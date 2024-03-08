b0401c595906f9f33353e4a1a4e6c08d31ddc7f0
eiwos <mahmoudi.otman@gmail.com>
2024-03-08 15:35:29 +0100

    Adding timing equal to crypt

--- src/core/crypt.ts
+++ src/core/crypt.ts
@@ -2,7 +2,7 @@
  *
  */
 import { promisify } from 'node:util';
-import { randomBytes, pbkdf2 } from 'node:crypto';
+import { randomBytes, pbkdf2, timingSafeEqual } from 'node:crypto';
 
 /**
  *
@@ -12,6 +12,7 @@ export const pbkdf2Async = promisify(pbkdf2);
 export async function generatePassword(password: string, salt?: string, iterations = 210_000) {
   const bytesPassword = Buffer.from(password);
   const bytesSalt = salt ? Buffer.from(salt, 'hex') : randomBytes(64);
+
   const bytesFinal = await pbkdf2Async(
     bytesPassword,
     bytesSalt,
@@ -24,4 +25,13 @@ export async function generatePassword(password: string, salt?: string, iteratio
     bytesSalt,
     bytesFinal
   }
+}
+
+export async function comparePassword(password: string, salt: string, raw: string, iterations = 210_000) {
+  const bytesPassword = await generatePassword(raw, salt, iterations);
+
+  return timingSafeEqual(
+    Buffer.from(password, 'hex'),
+    bytesPassword.bytesFinal
+  );
 }
